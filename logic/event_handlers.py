a02e2ec8ba32178bf3b3233928fa44ae872f6774
Diego Prokes Herbage <diego.prokes@gmail.com>
2024-01-16 16:48:53 -0300

    se programó la lógica para extraer el texto de documentos doc y docx

--- logic/event_handlers.py
+++ logic/event_handlers.py
@@ -1,11 +1,13 @@
 from tkinter import filedialog
 from pathlib import Path
+import docx
 
 class EventHandler:
     def __init__(self, main_window):
         # Referencia a la ventana principal para acceder a sus elementos
         self.main_window = main_window
         self.doc_list = []
+        self.text = ''
 
     def load_documents(self):
         # Función para cargar documentos
@@ -56,6 +58,53 @@ class EventHandler:
         if entry.get():
             entry.delete(0,len(entry.get()))
         self.main_window.output_directory_entry.insert(0,directorio)
-    
+
     def generate_text(self):
-        print("Generando...")
+        self.delete_text()
+        for document in self.doc_list:
+            file_path   = document[0]
+            if file_path.endswith('.docx'):
+                self.text = self.extract_text_from_docx(file_path)
+            elif file_path.endswith('.doc'):
+                self.text = self.extract_text_from_doc(file_path)
+            else:
+                raise ValueError("Unsupported file format")
+        print(self.text)
+    
+    def delete_text(self):
+        self.text=''
+        print(self.text)
+
+    def extract_text_from_docx(self, file_path):
+        doc = docx.Document(file_path)
+        text = "\n".join([para.text for para in doc.paragraphs])
+        return text
+        
+    def extract_text_from_doc(self, file_path):
+        # Esta función requiere que estés en un entorno Windows
+        try:
+            import win32com.client
+            word = win32com.client.Dispatch("Word.Application")
+            word.visible = False
+            wb = word.Documents.Open(file_path)
+            doc = word.ActiveDocument
+            text = doc.Range().Text
+            doc.Close()
+            word.Quit()
+            return text
+        except Exception as e:
+            print(f"Error al procesar el archivo DOC: {e}")
+            return ""
+        
+    def save_text_to_file(self, text, file_path):
+        try:
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(text)
+            print(f"Texto guardado con éxito en {file_path}")
+        except Exception as e:
+            print(f"Error al guardar el texto: {e}")
+    
+    def ask_save_file_and_write_text(self, extracted_text):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt")])
+        if file_path:
+            self.save_text_to_file(extracted_text, file_path)
